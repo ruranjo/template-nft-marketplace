e5a18ad59950ed0b839e1f055d79f3a91dff8d7a
ruranjo <rubens.rangel.job@gmail.com>
2023-06-14 08:26:15 -0400

    assets and section established

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import NavBar from "./navbar";
+import style from 'styled-components'
+
+export default function Home(){
+    return (
+        <>
+            <Section>
+                <NavBar/>
+            </Section>
+        </>
+    )
+}
+
+const Section = style.section`
+    margin: 0.5rem;
+    background-color: #232835;
+    border-radius: 1rem;
+    height: 100vh;
+`;
+
+
